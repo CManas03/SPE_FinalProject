47e37e1067573863146911a317c5a514c7413cf8
cmanas03 <chaitanya.manas@iiitb.ac.in>
2023-12-15 17:17:31 +0530

    logging using pygrok

--- PortfolioApp/src/components/AboutMe.jsx
+++ PortfolioApp/src/components/AboutMe.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './AboutMe.css';
 
 const AboutMe = () => {
-  const aboutMeText = "In Bangalore's vibrant mosaic, I thrive amidst diverse cultures, fluent in four languages that foster connections. Movies, cricket, and tennis ignite spirited dialogues, reflecting my zest for interaction.\n\nCollege sparked my journey in software engineering, unveiling the world of hackathons and full-stack development. Eagerly embracing machine learning and adaptable tech stacks, I seek knowledge without bounds.\n\nGoing beyond expectations, I delve into business intricacies, aiming to redefine standards and add unparalleled value. Amidst Student Affairs, I honed leadership orchestrating my college fest, while clubs meld my interests into impactful experiences.";
+  const aboutMeText = "In Bangalore's vibrant mosaic, I thrive amidst diverse cultures, fluent in four languages that foster connections. Movies, cricket, and tennis ignite spirited dialogues, reflecting my zest for interaction.\n\nCollege sparked my journey in software engineering, unveiling the world of hackathons and full-stack development. Eagerly embracing machine learning and adaptable tech stacks, I seek knowledge without bounds. My passion for learning and growth drives me to explore new avenues, from web development to data science.\n\nI am a firm believer in the power of technology to transform lives. I am excited to leverage my skills to create innovative solutions that make a difference.";
 
   return (
     <div id="aboutme" className="section aboutme-container">
